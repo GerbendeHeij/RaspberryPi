1f688fa026381a18b5c7042e576223748c6b61e2
Gerben <gcdeheij@gmail.com>
2015-08-25 00:09:05 +0200

    correct

--- Blink.py
+++ Blink.py
@@ -11,15 +11,15 @@ if __name__ == '__main__':
     connection = SerialManager()
     a = ArduinoTree(connection=connection)
     pin9 = a.pin.get(9)
-    pin10 = a.pin.get(9)
-    pin11 = a.pin.get(9)
+    pin10 = a.pin.get(10)
+    pin11 = a.pin.get(11)
     pin9.mode = 1
     pin10.mode = 1
     pin11.mode = 1
     pin9.write_digital_value(1)
-    time.sleep(10)
+    time.sleep(0.010)
     pin10.write_digital_value(1)
-    time.sleep(10)
+    time.sleep(0.010)
     pin11.write_digital_value(1)
     redpin = pin9.pwm
     greenpin = pin10.pwm
@@ -29,22 +29,22 @@ if __name__ == '__main__':
 
     while running:
         Input_text =input("Give the collor rightness of red");
-        if input == 'exit':
+        if Input_text == 'exit':
             running = False
         red = Input_text
         Input_text =input("Give the collor rightness of green");
-        if input == 'exit':
+        if Input_text == 'exit':
             running = False
         green = Input_text
         Input_text =input("Give the collor rightness of blue");
-        if input == 'exit':
+        if Input_text == 'exit':
             running = False
         blue = Input_text
 
         redpin.write_value(red)
-        time.sleep(10)
+        time.sleep(0.010)
         greenpin.write_value(green)
-        time.sleep(10)
+        time.sleep(0.010)
         bluepin.write_value(blue)
 
         print red
