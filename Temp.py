acbc9426daf647a633779e86ad498f014d238deb
Gerben <gcdeheij@gmail.com>
2015-09-06 17:37:17 +0200

    Temp sensor test

---
+++ Temp.py
@@ -0,0 +1,22 @@
+__author__ = 'gcdeh_000'
+
+from nanpy.arduinotree import ArduinoTree
+from nanpy import onewire
+from nanpy import dallastemperature
+
+__author__ = 'gcdeh_000'
+
+from nanpy.pwm import ArduinoPwmPin
+from nanpy.serialmanager import SerialManager
+import time
+
+
+if __name__ == '__main__':
+    connection = SerialManager()
+    a = ArduinoTree(connection=connection)
+    TempSensor = dallastemperature.DallasTemperature(4,connection=connection)
+
+    print "Start reading temp sensor"
+    time.sleep(0.010)
+    temp = TempSensor.getTempC
+    print temp
